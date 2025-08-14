593a30393846f53f26c3499f8efe078ef2a26fb8
Diw <mmvivix95@gmail.com>
2025-08-14 12:14:55 +0700

    lab05

---
+++ app/routes/components/Data/TermProject.tsx
@@ -0,0 +1,29 @@
+export const myTermProj = [
+    {
+        tpID : 1,
+        tpTitle: "Wed Technology",
+        tpSudject:"พัฒนาเว็บไซต์ [เกี่ยวกับ ม.จักรพงษ์ภูวนารถ]",
+        tpCover: "/images/8888.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491014-9/",
+        tpTeam: true
+        
+    },
+   {
+        tpID : 2,
+        tpTitle: "Wed Technology ",
+        tpSudject:"พัฒนาเว็บไซต์ [เกี่ยวกับ ม.จักรพงษ์ภูวนารถ] ",
+        tpCover: "/images/8888.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491014-9/",
+         tpTeam: false
+    },
+
+
+    {
+        tpID : 3,
+        tpTitle: "Wed Technology [เกี่ยวกับ ม.จักรพงษ์ภูวนารถ]",
+        tpSudject:"พัฒนาเว็บไซต์ [เกี่ยวกับ ม.จักรพงษ์ภูวนารถ] ",
+        tpCover: "/images/8888.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491014-9/", 
+        tpTeam: true
+    },
+];
